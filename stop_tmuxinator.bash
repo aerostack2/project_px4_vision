a7a14586645abd8a60da5df083052c66a6429c2e
Rafael Perez-Segui <rafa.perez.s98@gmail.com>
2024-08-02 14:05:32 +0200

    Upgrade stop_tmuxinator dettaching from tmux

--- stop_tmuxinator.bash
+++ stop_tmuxinator.bash
@@ -10,6 +10,11 @@ input=$1
 # Get the directory of the current script
 script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
+# Dettach from the current tmux session if inside one
+if [ -n "$TMUX" ]; then
+    tmux detach
+fi
+
 # Start each script in its own tmux session
 tmux new-session -d -s ground_station_stop "${script_dir}/stop_tmuxinator_ground_station.bash"
 tmux new-session -d -s as2_stop "${script_dir}/stop_tmuxinator_as2.bash" "$input"
